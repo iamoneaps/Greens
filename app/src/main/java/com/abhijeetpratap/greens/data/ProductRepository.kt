ddc4a8e2c12e49935669814b35e583c08ced7478
Akshay <akshay2104.singh@gmail.com>
2019-06-10 20:06:15 +0530

    added ProductDatabase.kt added ProductRepository.kt

---
+++ app/src/main/java/com/abhijeetpratap/greens/data/ProductRepository.kt
@@ -0,0 +1,32 @@
+package com.abhijeetpratap.greens.data
+
+import android.app.Application
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+
+class ProductRepository(application: Application) {
+    private val database: ProductDatabase = ProductDatabase.getInstance(application)
+    private val productDao: ProductDao = database.productDao()
+
+    val allUsers: LiveData<List<Product>> = productDao.allProducts
+
+    fun addUser(product: Product) {
+        InsertUser(productDao).execute(product)
+    }
+
+    fun deleteUser(product: Product) {
+        DeleteUser(productDao).execute(product)
+    }
+
+    class InsertUser(private val productDao: ProductDao) : AsyncTask<Product, Unit, Unit>() {
+        override fun doInBackground(vararg products: Product?) {
+            products[0]?.let { productDao.addProduct(product = it) }
+        }
+    }
+
+    class DeleteUser(private val productDao: ProductDao) : AsyncTask<Product, Unit, Unit>() {
+        override fun doInBackground(vararg products: Product?) {
+            products[0]?.let { productDao.deleteProduct(product = it) }
+        }
+    }
+}
